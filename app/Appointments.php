f9b8222a7870093b32992cfe1b4414f30e2dbab1
Unknown <earnest0987@gmail.com>
2018-08-14 15:04:24 +0800

    Layout of appointment calendar/some issues in the location of view and rules in models

---
+++ app/Appointments.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointments extends Model
+{
+    //
+    protected $table = 'appointments';
+	public $primaryKey = 'id';
+    public $timestamps = false;
+
+
+     public function rules()
+    {
+        return [
+            'appointment_name' => "required|min:5|max:30|unique:$this->table,appointment_name",
+            'appointment_start' => 'required|date|After:Yesterday',
+          	'appointment_end' => 'required'
+              
+        ];
+    }
+
+    public function customerRules(){
+            'lastname' => "required|max:30|string",
+            'firstname' => "required|max:30|string",
+            'middlename' => "required|max:30",
+            'contact' => 'required|min:2|max:60',
+            'email' => 'required|email'
+    }
+    
+    public function checkIfAppointmentExists()
+    {
+        return [
+            'appointments' => "required|exists:$this->table,id"
+        ];
+    }
+
+}
---
+++ resources/views/admin/transaction/appointments/create.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.layouts.app')
+
+@section('content-header')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <div class="container-fluid">
+      <h1 class="float-left">Service Appointment</h1>
+      <ol class="breadcrumb float-right">
+        <li class="breadcrumb-item"><a href="{{ url('dashboard') }}">Home</a></li>
+        <li class="breadcrumb-item">Service Appointment</li>
+      </ol>
+  </div><!-- /.container-fluid -->
+</section>
+@endsection
+
+@section('content-body')
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="card col-sm-12 mt-3 card-success">
+        <div class="card-header">
+          <h5> APPOINTMENT SCHEDULER<h5>
+        </div>
+        <div class="card-body">
+        	<form method="post" action="{{ url('appointments') }}" class="form-horizontal">
+	            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+	            @include('errors.alert')
+	            @include('admin.maintenance.appointments.form')
+	            <div class="form-group">
+	                <button type="submit" class="btn btn-success btn-block">Save</button>
+	            </div>
+	        </form>
+        </div>
+    </div>
+
+  </div>
+   <div class="card col-sm-12 mt-3 card-success">
+        <div class="card-header">
+          <h5> SERVICE CALENDAR<h5>
+        </div>
+        <div class="card-body">
+
+          {!! $calendar_details->calendar() !!}
+         
+        </div>
+    </div>
+
+  </div>
+</section>
+
+
+
+{!! $calendar_details->script() !!}
+
+@endsection
